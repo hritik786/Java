ea90cd0e64286396f04fb412545c4c9644a20d54
hritik786 <55953605+hritik786@users.noreply.github.com>
2023-03-04 00:09:46 +0530

    Add files via upload
    First Commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList();
+        Scanner scanner= new Scanner(System.in);
+        for(int i=1;i<=5;i++){
+            System.out.println("Please Enter the name");
+            String temp =scanner.next();
+            arrayList.add(temp);
+        }
+        for(String var: arrayList)
+            System.out.println(var);
+
+    }
+}
